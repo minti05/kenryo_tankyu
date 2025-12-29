7890324d090e5f53a5856121dcd23d9b4747e674
tsumu <goto.aoi.516@gmail.com>
2025-12-29 18:15:06 +0900

    直接インポートへの変更

--- lib/features/search/presentation/screens/search_page.dart
+++ lib/features/search/presentation/screens/search_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:kenryo_tankyu/core/constants/const.dart';
+import 'package:kenryo_tankyu/core/constants/work/info_value.dart';
+import 'package:kenryo_tankyu/core/constants/work/sub_category_value.dart';
 import 'package:kenryo_tankyu/features/search/data/datasources/search_history_db.dart';
 import 'package:kenryo_tankyu/core/constants/work/category_value.dart';
 import 'package:kenryo_tankyu/features/search/domain/models/search.dart';
